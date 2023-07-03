894e80e440e8ccbf6d8d61e0e6453ff3cb696f86
Viviane Martini <martini.viviane@gmail.com>
2023-07-03 10:45:37 -0300

    add play function

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,51 @@
-
 const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.pause')
 const buttonMoreTime = document.querySelector('.more-time')
 const buttonLessTIme = document.querySelector('.less-time')
+let minutesDisplay = document.querySelector('.minutes')
+let secondsDisplay = document.querySelector('.seconds')
+let minutes
+let timerTimeOut
 
-//steps
-// pegar minutos e ssegundos
-//eles devem diminuir
-//o play co clicar funciona 
+function updateTimerDisplay(minutes, seconds){
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
+  secondsDisplay.textContent = String(seconds).padStart(2, '0')
+
+}
+
+
+function countdown(){
+  timerTimeOut = setTimeout(function(){
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+  
+    if(minutes <= 0){
+      return
+    }
+
+    if(seconds <= 0) {
+      seconds = 10
+
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+
+    countdown()
+  }, 1000)
+}
+
+buttonPlay.addEventListener('click',function(){
+  countdown()
+})
+
+buttonStop.addEventListener('click', function(){
+  clearTimeout(timerTimeOut)
+})
 
 
 /* 
-seleçaõ de botão
+seleçaõ de botão de som
 const menuItems = document.querySelectorAll('.sound__item')
 let menuItemActive = document.querySelector('.sound--active')
 
@@ -28,3 +62,11 @@ function buttonClick() {
   }
 }
  */
+
+/* function moreTime (){
+
+}
+
+function lessTime(){
+
+} */
